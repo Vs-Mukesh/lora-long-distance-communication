73089e4ef4f470a970f0da88f34ca981cb96a9d1
Vs-Mukesh <vsmukesh.07@gmail.com>
2026-03-30 15:24:32 +0530

    Implement real-time data visualization for temperature and humidity
    Added functionality to read temperature and humidity data from a serial port and visualize it using matplotlib.

--- python/data_visualization.py
+++ python/data_visualization.py
@@ -1,1 +1,30 @@
+import serial
+import matplotlib.pyplot as plt
 
+ser = serial.Serial('COM3', 9600)
+
+temps = []
+humidity = []
+
+while True:
+    line = ser.readline().decode().strip()
+    print(line)
+
+    try:
+        values = line.split(',')
+
+        if len(values) == 4:
+            t = float(values[0])
+            h = float(values[1])
+
+            temps.append(t)
+            humidity.append(h)
+
+            plt.clf()
+            plt.plot(temps, label="Temperature")
+            plt.plot(humidity, label="Humidity")
+            plt.legend()
+            plt.pause(0.1)
+
+    except:
+        pass
